ba5791f6e1aeba9c83dd98a693dc909f6de8fb23
Henry J Fender <HEFE002@hcso.fr.co.hennepin.mn.us>
2017-05-30 15:43:44 -0500

    Creating repo for JPassion homework

---
+++ javase_progbasics/MyGreatestValueProject2/src/GreatestValue.java
@@ -0,0 +1,24 @@
+
+public class GreatestValue {
+	/**
+	 * @param args the command line arguments
+	 */
+	
+	public static void main(String[] args) {
+		//Declare the numbers
+		int num1 = 10;
+		int num2 = 23;
+		int num3 = 5;
+		int max = 0;
+		
+		//Determines the highest number
+		max = (num1<num2)?num1:num2;
+		max = (max<num3)?max:num3;
+		
+		//prints the output on the screen
+		System.out.println("number 1 = "+num1);
+		System.out.println("number 2 = "+num2);
+		System.out.println("number 3 = "+num3);
+		System.out.println((max<10)?"The smallest number is less than 10!":"The smallest number is greater than or equla to 10!");
+	}
+}
---
+++ javase_progenv/MyHelloProject2/src/jpassion/Homework.java
@@ -0,0 +1,8 @@
+package jpassion;
+
+public class Homework {
+	public static void main(String[] args) {
+		System.out.println("Hello, this is the first line from MyHelloProject2");
+		System.out.println("This is the second line from the same project.");
+	}
+}
