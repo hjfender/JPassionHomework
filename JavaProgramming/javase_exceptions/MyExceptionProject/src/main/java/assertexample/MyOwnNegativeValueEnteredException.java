accf3d8835422507e6c644bdd473bbabe36961d5
Henry J Fender <HEFE002@hcso.fr.co.hennepin.mn.us>
2017-06-15 15:45:54 -0500

    Complete Java Exceptions project

---
+++ JavaProgramming/javase_exceptions/MyExceptionProject/src/main/java/assertexample/MyOwnNegativeValueEnteredException.java
@@ -0,0 +1,8 @@
+package assertexample;
+
+public class MyOwnNegativeValueEnteredException extends ArithmeticException {
+	
+	public MyOwnNegativeValueEnteredException(String s){
+		super(s);
+	}
+}
---
+++ JavaProgramming/javase_exceptions/MyExceptionProject/src/main/java/assertexample/MyOwnZeroValueEnteredException.java
@@ -0,0 +1,5 @@
+package assertexample;
+
+public class MyOwnZeroValueEnteredException extends ArithmeticException {
+
+}
